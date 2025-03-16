5180b55412df921fb707c503d26a7b0366edd3fb
eu <eu.nyao.9@gmail.com>
2025-03-16 20:24:30 +0900

    不要ファイル削除、layout設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  // ↓最終的にデプロイされるURL。canonicalタグに使用
+  site: "https://astro.build/",
+  // ↓ビルド時にHTMLを圧縮しない
+  compressHTML: false,
+  build: {
+    // ↓ソースフォルダに作成した通りのファイルパスでHTMLファイルを生成
+    format: "preserve",
+  },
+  // ↓開発時に末尾のスラッシュを必須にする
+  trailingSlash: "always",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  integrations: [react()],
 });
